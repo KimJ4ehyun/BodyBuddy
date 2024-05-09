55244c0accf1bdee227167930282bb92daeaddda
yelim99 <minyl8234@gmail.com>
2024-05-09 20:16:23 +0900

    Init: 초기 프로젝트 설정 수정

--- BodyBuddyProject/src/main/java/com/bodybuddy/fit/controller/UserController.java
+++ BodyBuddyProject/src/main/java/com/bodybuddy/fit/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.bodybuddy.fit.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
 
 }
--- BodyBuddyProject/src/main/java/com/bodybuddy/fit/model/service/UserServiceImpl.java
+++ BodyBuddyProject/src/main/java/com/bodybuddy/fit/model/service/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.bodybuddy.fit.model.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl implements UserService {
 
 }
