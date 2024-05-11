a54c04a8d272945aada2e595e97e89da435d6e79
KimJ4ehyun <kimjaehyun158@gmail.com>
2024-05-11 14:44:30 +0900

    Chore: Vue 초기 파일 추가

---
+++ Front/BodyBuddy/src/components/common/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink :to="{ name: 'login' }">로그인</RouterLink> |
+        <RouterLink :to="{ name: 'join' }">회원가입</RouterLink> 
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
---
+++ Front/BodyBuddy/src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import JoinView from '@/views/JoinView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/join',
+      name: 'join',
+      component: JoinView
+    },
+  ]
+})
+
+export default router
