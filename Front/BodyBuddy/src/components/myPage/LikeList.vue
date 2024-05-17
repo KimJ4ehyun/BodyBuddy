c62f816d59e39499e1d71b0ae19d7d7559211c12
yelim99 <156084622+yelim99@users.noreply.github.com>
2024-05-18 00:25:35 +0900

    Feat: 루틴 게시판에서  찜하기 완료

--- Front/BodyBuddy/src/components/myPage/LikeList.vue
+++ Front/BodyBuddy/src/components/myPage/LikeList.vue
@@ -1,10 +1,33 @@
 <template>
     <div class="likeContainer">
         <h4>찜 목록</h4>
+        <div class="likeBox" v-for="wish in store.wishList" :key="wish.wishId">
+            <!-- <BoardListOne :board-one="wish.boardInfo" /> -->
+        </div>
     </div>
 </template>
 
 <script setup>
+    import BoardListOne from '@/components/routineBoard/BoardListOne.vue'
+    import { useBoardStore } from '@/stores/board';
+    import { useWishStore } from '@/stores/wish';
+    import { onMounted, ref, computed } from 'vue'
+
+
+    const store = useWishStore()
+    const boardStore = useBoardStore()
+
+    console.log(store.wishList)
+    // onMounted (() => {
+    //     store.getwishList()
+    // })
+
+    // onMounted(async () => {
+    //     await Promise.all(store.wishList.map(async wish => {
+    //         await boardStore.getBoard(wish.routineId);
+    //         wish.boardInfo = boardStore.board;  // 각 wish에 board 정보 추가
+    //     }));
+    // });
 
 </script>
 
@@ -12,6 +35,11 @@
     .likeContainer {
         border: 1px solid gray;
         width: 70%;
-        margin: 20px auto;
+        margin: 50px auto;
+    }
+
+    .likeBox {
+        border: 1px solid lightblue;
+        
     }
 </style>
